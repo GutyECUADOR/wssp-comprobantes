0c4451dc6d87189b85e6f64325ccc706d52265cb
José L. Gutiérrez <gutiecuador@gmail.com>
2026-03-30 23:04:40 -0500

    feat: enhance DatabaseSeeder to create admin user and improve local environment checks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Crear el usuario administrador de forma manual (Seguro para producción)
+        User::updateOrCreate(
+            ['email' => 'admin@admin.com'], // Busca si ya existe por email
+            [
+                'name' => 'Administrador',
+                'password' => Hash::make(env('ADMIN_PASSWORD')), // Hasheado correctamente
+                'email_verified_at' => now(),
+            ]
+        );
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
@@ -19,6 +31,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(Usuario::class);
+        if (app()->environment('local')) {
+            $this->call(Usuario::class);
+        }
     }
 }
